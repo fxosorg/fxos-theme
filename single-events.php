adbab6b32b570de0dbcde62853ecbaa90588ddd2
tommmmy <tommmmmmmmy@gmail.com>
2013-05-09 13:31:43 +0900

    adjunsting

--- single-events.php
+++ single-events.php
@@ -9,6 +9,20 @@
 
 get_header(); ?>
 
+			<section class="org">
+				<h1><i class="icon-bullhorn"></i>イベント詳細</h1>
+
+
+				<article class="box">
+					<h2><?php the_title(); ?></h2>
+					<p class="date"><?php echo get_post_meta($post->ID,'開催日時',TRUE); ?></p>
+					<div class="wrap">
+						<p class="thumb"><a href="#"><img src="assets/img/mozillathon1.jpg" alt="mozillathon1" width="200"></a></p>
+						<p class="text"><?php echo mb_substr(strip_tags($post-> post_content),0,200).'...'; ?></p>
+					</div>
+				</article>
+
+			</section>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
--- single_.php
+++ single_.php
@@ -9,7 +9,6 @@
 
 get_header(); ?>
 
-
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 
